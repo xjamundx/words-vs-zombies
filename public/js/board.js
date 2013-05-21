6ce98891d66cb4ce7ef73affe52aeeddf72c42f5
Jamund Ferguson <jamund@gmail.com>
2013-05-21 11:51:48 -0700

    Moved to multi-controller approach, fixed CSS for retina, etc.

--- public/js/board.js
+++ public/js/board.js
@@ -1,27 +1,49 @@
-angular.module('Words', []).
-  directive('tile', function() {
-    return {
-      restrict: 'E',
-      replace: true,
-      templateUrl: '/templates/square.html'
-    }
-  }).
-  directive('tray', function() {
-    return {
-      restrict: 'E',
-      replace: true,
-      templateUrl: '/templates/tray.html'
+var app = angular.module('Words', []);
+
+// tile directive
+app.directive('tile', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: '/templates/square.html'
+  }
+})
+
+// tray directive
+app.directive('tray', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: '/templates/tray.html'
 	}
 });
-  
-function BoardCtrl($scope) {
 
-	'use strict';
-	
-	var WIDTH = 11;
-	var HEIGHT = 11;
-	var NUM_TILES = WIDTH * HEIGHT;
-	var TILES_PER_TRAY = 7;
+// services to control the game
+app.factory('game', function() {
+	var game = {
+		player: 1,
+		isRunning: false,
+		numPlayers: 2
+	};
+	return {
+		end: function() {
+			game.isRunning = false;
+			game.player = 1;
+		},
+		start: function () {
+			game.isRunning = true;
+		},
+		next: function() {
+			game.player++; 
+			if (game.player > game.numPlayers) game.player = 1;
+		},
+		game: game
+	}
+});
+
+// service to control the bag
+app.service('bag', function() {
+	var letters = [];
 	var LETTERS = {
 		  a: {points: 1, count: 3}
 		, b: {points: 1, count: 3}
@@ -49,50 +71,76 @@ function BoardCtrl($scope) {
 		, x: {points: 1, count: 1}
 		, y: {points: 1, count: 3}
 		, z: {points: 1, count: 1}
-	    , '☠': {points: 100, count: 1}
+    , '☠': {points: 100, count: 1}
+	};
+
+	function getTiles(num) {
+		var tiles = [];
+		var letter;
+		for (var i = 0; i < num; i++) {
+			letter = letters.pop();
+			if (letter) tiles.push(letter);
+		}
+		return tiles;
 	}
+
+	function shuffle() {
+		letters.sort(randomish);
+	}
+
+	function randomish() {
+		return 0.5 - Math.random();
+	}
+
+	function getLetters() {
+		var letters = [];
+		for (var letter in LETTERS) {
+			for (var i = 0; i < LETTERS[letter].count; i++) {
+				letters.push({letter: letter, number: LETTERS[letter].points});
+			}
+		}
+		return letters;
+	}
+
+	function returnTiles(tiles) {
+		letters.concat(tiles.splice(0, tiles.length));
+	}
+
+	letters = getLetters();
+	shuffle();
+
+	return {
+		letters: letters,
+		shuffle: shuffle,
+		getTiles: getTiles,
+		returnTiles: returnTiles
+	};
+
+});
+
+app.factory('trays', function() {
+	return [{tiles:[]}, {tiles:[]}];
+});
+  
+app.controller('BoardCtrl', function($scope) {
 	
+	var WIDTH = 11;
+	var HEIGHT = 11;
+	var NUM_TILES = WIDTH * HEIGHT;
+
 	// model
-	$scope.tiles = getTiles();	
-	$scope.letters = getLetters();
-	$scope.trays = [{tiles:[]}, {tiles:[]}];
+	$scope.tiles = getTiles();
 	$scope.selected = [];
-	$scope.activeTray = null;
-	$scope.activeIndex = 0;
-	$scope.isRunning = false;
-	$scope.player = 1;
-	$scope.numPlayers = 2;
 	
 	// actions
 	$scope.pickLetter = function() {
 		$scope.setTile(1, 1, $scope.letters[$scope.letters.length - 1]);
 		return $scope.letters.pop();
 	};
-	
-	$scope.refill = function(tray) {
-		var needed = Math.max(TILES_PER_TRAY - tray.tiles.length, 0)
-		var tiles = getYourTiles(needed);
-		tray.tiles = tray.tiles.concat(tiles);
-	};
-	
+		
 	$scope.setTile = function(x, y, letter) {
 		$scope.tiles.splice(x * y - 1, 1, letter);
 	};
-	
-	$scope.shuffle = function() {
-		$scope.letters.sort(randomish);
-	};
-	
-	$scope.endGame = function() {
-		$scope.isRunning = false;
-	}
-	
-	$scope.next = function() {
-		$scope.trays[$scope.player - 1].status = '';
-		$scope.player++;
-		if ($scope.player > $scope.numPlayers) $scope.player = 1;
-		$scope.trays[$scope.player - 1].status = 'active';
-	}
 	
 	$scope.place = function(tile) {
 		if (!tile) return;
@@ -102,54 +150,8 @@ function BoardCtrl($scope) {
  		tile.number = sel.number;
  		tile.status = 'played';
  		$scope.selected[0] =  null; // remove selected tile
- 		$scope.activeTray.tiles.splice($scope.activeIndex, 1); // remove from tray
+ 		// $scope.activeTray.tiles.splice($scope.activeIndex, 1); // remove from tray
 	};
-	
-	$scope.select = function(tile) {
-		var wasSelected = tile.selected;
-		$scope.activeTray = this.$parent.tray;
-		$scope.activeIndex = this.$index;
-		this.$parent.tiles.forEach(function(t) {
-			t.selected = false;
-		});
-		tile.selected = !wasSelected;
-		tile.status = tile.selected ? 'selected' : '';
-		$scope.selected[0] = tile.selected ? tile : null;
-	};
-	
-	$scope.startGame = function() {
-		$scope.isRunning = true;
-		$scope.trays[0].status = 'active';
-		$scope.trays.forEach(function(tray, i) {
-			$scope.refill(tray);
-		});
-	};
-	
-	function getYourTiles(num) {
-		num =  typeof num !== 'undefined' ? num : TILES_PER_TRAY;
-		var tiles = [];
-		var letter;
-		for (var i = 0; i < num; i++) {
-			letter = $scope.letters.pop()
-			if (letter) tiles.push(letter);
-		}
-		return tiles;
-	}
-	
-	function getLetters() {
-		var letters = [];
-		for (var letter in LETTERS) {
-			for (var i = 0; i < LETTERS[letter].count; i++) {
-				letters.push({letter: letter, number: LETTERS[letter].points});
-			}
-		}
-		letters.sort(randomish);
-		return letters;
-	}
-	
-	function randomish() {
-		return 0.5 - Math.random();
-	}
 	
 	function getTiles() {
 		var middle = Math.floor(NUM_TILES / 2);
@@ -161,4 +163,89 @@ function BoardCtrl($scope) {
 		return tiles;
 	}
 	
-}
+});
+
+app.controller('BagCtrl', function($scope, bag) {
+	$scope.letters = bag.letters;
+	$scope.shuffle = function() {
+		// $scope.$apply(); // what is this?		
+		bag.shuffle();
+	}
+  // $scope.$watch(bag.letters, function(old, letters, $scope) {
+  // 	console.log('watching bag.letters', arguments)
+  // 	if (letters) $scope.letters = letters;
+  // });
+});
+
+app.controller('TraysCtrl', function($scope, game, bag, trays) {
+
+	var TILES_PER_TRAY = 7;
+
+	$scope.trays = trays;
+
+	$scope.shuffle = function() {
+		$scope.trays[game.game.player - 1].tiles.sort(function() {
+			return 0.5 - Math.random();
+		})
+	};
+
+	$scope.refill = function(tray) {
+		var needed = Math.max(TILES_PER_TRAY - tray.tiles.length, 0)
+		var tiles = bag.getTiles(needed);
+		tray.tiles = tray.tiles.concat(tiles);
+	};
+
+	$scope.play = function() {
+		$scope.trays.forEach(function(tray) {
+			$scope.refill(tray);
+		});
+
+		// kinda weird
+		$scope.startGame();
+	};
+
+	$scope.end = function() {
+		// return all the times
+		$scope.trays.forEach(function(tray) {
+			bag.returnTiles(tray.tiles);
+		});
+
+		// kinda weird
+		$scope.endGame();
+	};
+
+	function clearAll() {
+		$scope.trays[game.game.player - 1].tiles.forEach(function(tile) {
+			tile.selected = false;
+		});
+	};
+	
+	$scope.select = function(tile) {
+		clearAll();
+		tile.selected = !tile.selected;
+	};
+
+	$scope.fill = function($index) {
+		$scope.refill($scope.trays[$index]);
+	};
+
+});
+
+app.controller('GameCtrl', function($scope, game) {
+
+	// adding the service to the scope
+	$scope.game = game.game;
+
+	$scope.next = function() {
+		game.next();
+	};
+
+	$scope.endGame = function() {
+		game.end();
+	};
+
+	$scope.startGame = function() {
+		game.start();
+	};
+
+});
